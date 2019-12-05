351aa22315b2452ebe41ed933f26a5bb8d1a26ef
Vlad F <vlad.faget@outlook.com>
2019-12-05 12:11:54 +0100

    Modified Read method

--- Model/Inventory.cs
+++ Model/Inventory.cs
@@ -17,23 +17,9 @@ namespace Pizza_Ani_Time.Model
         //Constructor
         public Inventory()
         {
-            //File read
-            try
-            {
-                StreamReader sr = File.OpenText("");
-                while (!sr.EndOfStream)
-                {
-                    string[] line = sr.ReadLine().Split(';');
-                    Product p = new Product(line[0], line[1], double.Parse(line[2]), line[3]);
-                    AllAvailableProducts.Add(p);
-                }
-                sr.Close();
-            }
-            //File read error
-            catch
-            {
-                dbError();
-            }
+            
+            DataCreator();
+            ReadData();
             
         }
 
@@ -42,11 +28,58 @@ namespace Pizza_Ani_Time.Model
             return AllAvailableProducts;
         }
 
+        private async void ReadData()
+        {
+            var ReadTask = ReadFile();
+            string Text = await ReadTask;
+
+            try
+            {
+                string[] line = Text.Split(";");
+                for (int i = 0; i < line.Count(); i++)
+                {
+                    Product p = new Product(line[i], line[i + 1], double.Parse(line[i + 2]), line[i + 3]);
+                    AllAvailableProducts.Add(p);
+                    i = i + 3;
+                }
+            }
+            catch
+            {
+                dbError();
+            }
+        }
+
+        private async Task<string> ReadFile()
+        {
+            string rawtext;
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            Windows.Storage.StorageFile DataFile = await storageFolder.CreateFileAsync("Data.txt", Windows.Storage.CreationCollisionOption.OpenIfExists);
+            var stream = await DataFile.OpenAsync(Windows.Storage.FileAccessMode.Read);
+            ulong size = stream.Size;
+            using (var inputStream = stream.GetInputStreamAt(0))
+            {
+                using (var dataReader = new Windows.Storage.Streams.DataReader(inputStream))
+                {
+                    uint numBytesLoaded = await dataReader.LoadAsync((uint)size);
+                    rawtext = dataReader.ReadString(numBytesLoaded);
+                    
+                }
+            }
+            return rawtext;
+        }
+
         //File read error message
         private async void dbError()
         {
             var messageDialog = new MessageDialog("Database error.");
             await messageDialog.ShowAsync();
+        }
+
+        private async void DataCreator()
+        {
+            Windows.Storage.StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            Windows.Storage.StorageFile DataFile = await storageFolder.CreateFileAsync("Data.txt", Windows.Storage.CreationCollisionOption.OpenIfExists);
+            await Windows.Storage.FileIO.WriteTextAsync(DataFile, "pizza;;22;none;");
         }
     }
 }
